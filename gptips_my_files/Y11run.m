ca1698e3056c157f45365aa099bb12bfa389e921
Sagar Kandpal <sagariit.kanpur1@gmail.com>
2021-07-17 13:31:25 +0530

    Add files via upload

---
+++ gptips_my_files/Y11run.m
@@ -0,0 +1,48 @@
+%run MGGP on Experiment data for Dielectric Constant
+clear;
+clc;
+disp('>>gp=rungp(@Y11_config);');
+disp('Press a key to continue');
+disp(' ');
+pause;
+gp = rungp(@Y11_config);
+disp(' ');
+disp('Evaluate the best individual of');
+disp('the runs on the fitness function using:');
+disp('>>runtree(gp,''best'');');
+disp('Press a key to continue');
+disp(' ');
+pause;
+runtree(gp,'best');
+if gp.info.toolbox.symbolic
+    
+    disp(' ');
+    disp('Next, use the the GPPRETTY command on the best individual: ');
+    disp('>>gppretty(gp,''best'')');
+    disp('Press a key to continue');
+    disp(' ');
+    pause;
+    
+    gppretty(gp,'best');
+     disp(' ');
+end
+disp('Next, use the the DRAWTREE command: ');
+  disp('>>drawtrees(gp,''best'')');
+  disp('Press a key to continue');
+  disp(' ');
+  pause;
+  drawtrees(gp,'best');
+  disp(' ');
+    pause;
+if gp.info.toolbox.symbolic
+    disp('Finally, an HTML report listing the models on the Pareto optimal front');
+    disp('of model expressional complexity and performance can be generated using');
+    disp('the PARETOREPORT function.');
+    disp('>>paretoreport(gp)');
+    disp(' ');
+    disp('Press a key to continue');
+    disp(' ');
+    pause;
+    paretoreport(gp);
+    disp('end');
+end
---
+++ gptips_my_files/Y1_config.m
@@ -0,0 +1,40 @@
+function gp=Y1_config(gp)
+%composite- Y1
+gp.runcontrol.pop_size=100;                     
+gp.runcontrol.num_gen=100;		
+gp.runcontrol.verbose=20;
+gp.fitness.fitfun=@regressmulti_fitfun;               
+gp.fitness.minimisation=true; 
+gp.runcontrol.showBestInputs = true;
+gp.runcontrol.runs = 1;
+
+gp.selection.method='tour';
+gp.selection.tournament.size=15;
+gp.selection.elite_fraction = 0.3;
+
+%fitness
+%gp.fitness.terminate = true;
+%gp.fitness.terminate_value =0.3;
+
+%Load the variables xtrain, ytrain, xtest and ytest and assign to gp structure
+load mydataY1;
+gp.userdata.xtrain=xtrain;
+gp.userdata.ytrain=ytrain;
+gp.userdata.xtest=xtest;
+gp.userdata.ytest=ytest;
+%gp.userdata.xval = xval;
+%gp.userdata.yval = yval;
+%Tell GPTIPS to call validation set once per generation
+%gp.userdata.user_fcn = @regressmulti_fitfun_validate;
+%Define the number of inputs
+gp.nodes.inputs.num_inp = size(gp.userdata.xtrain,2);
+
+%Enable multiple gene mode and set max number of genes per individual.
+gp.genes.multigene=true;              
+gp.genes.max_genes=12;
+gp.genes.max_depth=6;
+
+%Define function nodes
+gp.nodes.functions.name={'times','minus','plus'};
+% 'times','minus','plus','log','tanh','exp','square'
+end
